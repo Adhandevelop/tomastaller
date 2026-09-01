7f8bed7df164ba51d4da23726e201ee6c5692634
Adham <adham@users.noreply.github.com>
2026-08-31 19:41:41 -0500

    change

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const avgGeneral = document.getElementById('avgGeneral');
 const approvedSubjects = document.getElementById('approvedSubjects');
 
 const validUser = 'jtabril@ucompensar.edu.co';
-const validPassword = 'T_osaju2204*#';
+const validPassword = 'T_osaju2204*#123';
 
 function showApp() {
   loginScreen.classList.add('hidden');
